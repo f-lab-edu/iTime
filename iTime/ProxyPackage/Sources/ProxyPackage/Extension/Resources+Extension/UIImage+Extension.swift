f2d6c9c4e92c44d1e52fe7fbd55e2423f5e093bf
sangheon <sangheon0724@gmail.com>
2023-11-09 20:19:58 +0900

    [v 1.0.0 ] [feat: 리소스 코드 추가 ]

---
+++ iTime/ProxyPackage/Sources/ProxyPackage/Extension/Resources+Extension/UIImage+Extension.swift
@@ -0,0 +1,26 @@
+//
+//  File.swift
+//  
+//
+//  Created by 이상헌 on 11/9/23.
+//
+
+import UIKit
+
+extension UIImage {
+  
+  // MARK: - Common
+  
+  public static let calendal: UIImage = .init(resource: .calendar)
+  public static let exit: UIImage = .init(resource: .exit)
+  public static let gearFill: UIImage = .init(resource: .gearFill)
+  public static let off: UIImage = .init(resource: .off)
+  public static let on: UIImage = .init(resource: .on)
+  
+  // MARK: - Control
+  
+  public static let redStopButton: UIImage = .init(resource: .btnStopRed)
+  public static let stopButton: UIImage = .init(resource: .btnStop)
+  public static let tagFill: UIImage = .init(resource: .tagFill)
+  public static let tagLined: UIImage = .init(resource: .tagLined)
+}
