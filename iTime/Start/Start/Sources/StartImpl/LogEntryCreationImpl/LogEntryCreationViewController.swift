46182d3ab39e5f26fac8981bd63fb9e74e6e6d3f
sangheon <sangheon0724@gmail.com>
2023-11-10 10:18:39 +0900

    [v 1.0.0 ] [feat: LogEntryCreationViewController 하위 뷰 및 Constraints 추가 ]

--- iTime/Start/Start/Sources/StartImpl/LogEntryCreationImpl/LogEntryCreationViewController.swift
+++ iTime/Start/Start/Sources/StartImpl/LogEntryCreationImpl/LogEntryCreationViewController.swift
@@ -26,6 +26,11 @@ final class LogEntryCreationViewController:
   private enum Metric {
     static let todayDateBarHeight: CGFloat = 52
     static let encouragingBoxViewHeight: CGFloat = 60
+    static let buttonsLeadingTrailingMargin: CGFloat = 24
+    static let buttonsHeight: CGFloat = 52
+    static let editorRoutingButtonTopMargin: CGFloat = 58
+    static let startButtonsTopMargin: CGFloat = 12
+    static let startButtonsMinimumBottomMargin: CGFloat = 12
   }
   
   // MARK: - Properties
@@ -40,7 +45,24 @@ final class LogEntryCreationViewController:
   
   private lazy var encouragingBoxView = EncouragingBoxView()
   
+  private lazy var frameView = UIView().builder
+    .backgroundColor(.blue)
+    .build()
   
+  private lazy var editorRoutingButton = UIButton().builder
+    .backgroundColor(.black90)
+    .with {
+      $0.setTitle("내가 지금 할 것은...", for: .normal)
+      $0.setTitleColor(.black60, for: .normal)
+    }
+    .build()
+  
+  private lazy var startButton = UIButton().builder
+    .backgroundColor(.pointGreen)
+    .with {
+      $0.setImage(.add, for: .normal)
+    }
+    .build()
   
   // MARK: - View LifeCycle
   
@@ -58,6 +80,9 @@ extension LogEntryCreationViewController {
   private func setupUI() {
     view.addSubview(todayDateBar)
     view.addSubview(encouragingBoxView)
+    view.addSubview(frameView)
+    view.addSubview(editorRoutingButton)
+    view.addSubview(startButton)
     view.backgroundColor = .black100
     
     layout()
@@ -66,6 +91,9 @@ extension LogEntryCreationViewController {
   private func layout() {
     makeTodayDateBarConstraints()
     makeEncouragingBoxViewConstraints()
+    makeFrameViewConstraints()
+    makeEditorRoutingButtonConstraints()
+    makeStartButtonConstraints()
   }
   
   private func makeTodayDateBarConstraints() {
@@ -81,6 +109,29 @@ extension LogEntryCreationViewController {
       $0.top.equalTo(todayDateBar.snp.bottom)
       $0.leading.trailing.equalToSuperview()
       $0.height.equalTo(Metric.encouragingBoxViewHeight)
+    }
+  }
+  
+  private func makeFrameViewConstraints() {
+    frameView.snp.makeConstraints {
+      $0.center.equalToSuperview()
+      $0.size.equalTo(200)
+    }
+  }
+  
+  private func makeEditorRoutingButtonConstraints() {
+    editorRoutingButton.snp.makeConstraints {
+      $0.leading.trailing.equalToSuperview().inset(Metric.buttonsLeadingTrailingMargin)
+      $0.top.equalTo(frameView.snp.bottom).offset(Metric.editorRoutingButtonTopMargin)
+      $0.height.equalTo(Metric.buttonsHeight)
+    }
+  }
+  
+  private func makeStartButtonConstraints() {
+    startButton.snp.makeConstraints {
+      $0.height.leading.trailing.equalTo(editorRoutingButton)
+      $0.top.equalTo(editorRoutingButton.snp.bottom).offset(Metric.startButtonsTopMargin)
+      $0.bottom.greaterThanOrEqualToSuperview().offset(-Metric.startButtonsMinimumBottomMargin).priority(.high)
     }
   }
 }
