761882e3d2b096a8ecde5e890fbbdedc35254f06
sangheon <sangheon0724@gmail.com>
2023-11-11 13:50:30 +0900

    리소스 코드 추가 (#24)
    * [v 1.0.0 ] [feat: 리소스 코드 추가 ]
    
    * [v 1.0.0 ] [fix: 리뷰 반영- 오타 수정]

---
+++ iTime/ProxyPackage/Sources/ProxyPackage/Extension/Resources+Extension/UIColor+Extension.swift
@@ -0,0 +1,21 @@
+//
+//  File.swift
+//  
+//
+//  Created by 이상헌 on 11/9/23.
+//
+
+import UIKit
+
+extension UIColor {
+  public static let black10: UIColor = .black10
+  public static let black20: UIColor = .black20
+  public static let black30: UIColor = .black30
+  public static let black40: UIColor = .black40
+  public static let black50: UIColor = .black50
+  public static let black60: UIColor = .black60
+  public static let black70: UIColor = .black70
+  public static let black80: UIColor = .black80
+  public static let black90: UIColor = .black90
+  public static let black100: UIColor = .black100
+}
