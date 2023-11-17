13d50b7757cf998cf52638ae2e1b9ec51b8af0f1
sangheon <sangheon0724@gmail.com>
2023-11-17 19:02:46 +0900

    [v 1.0.0 ] [feat: flow layout 누락된 것 추가 ]

--- iTime/Feature/Sources/StartImpl/LogEntryCreationImpl/BookmarkEditor/Components/ItemHistorySectionView.swift
+++ iTime/Feature/Sources/StartImpl/LogEntryCreationImpl/BookmarkEditor/Components/ItemHistorySectionView.swift
@@ -39,7 +39,8 @@ final class ItemHistorySectionView: BaseView {
   private lazy var adapter = BookmarkTagsCollectionViewAdapter(
     collectionView: ItemHistoryCollectionView,
     adapterDataSource: listener,
-    delegate: listener
+    delegate: listener, 
+    alignedCollectionViewFlowLayout: LeadingAlignedCollectionViewFlowLayout()
   )
   
   // MARK: - properties
