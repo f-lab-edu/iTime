6b3050a2f8a3b2ac1ed71012dd7b497e137e927a
sangheon <sangheon0724@gmail.com>
2023-11-21 14:00:27 +0900

    [v 1.0.0 ] [fix: 리뷰 반영 - 네이밍 변경 ]

--- iTime/Domain/Sources/BookmarkUsecase/BookmarkUsecase.swift
+++ iTime/Domain/Sources/BookmarkUsecase/BookmarkUsecase.swift
@@ -10,8 +10,8 @@ import RxSwift
 // MARK: - BookmarkUsecase
 
 public protocol BookmarkUsecase {
-  func update(with bookmarks: [Bookmark]) -> Single<Void>
-  func append(with bookmark: Bookmark) -> Single<Void>
-  func remove(with bookmark: Bookmark) -> Single<Void>
+  func updateBookmarks(with bookmarks: [Bookmark]) -> Single<Void>
+  func appendBookmark(_ bookmark: Bookmark) -> Single<Void>
+  func removeBookmark(_ bookmark: Bookmark) -> Single<Void>
   func bookmarks() -> Single<[Bookmark]>
 }
