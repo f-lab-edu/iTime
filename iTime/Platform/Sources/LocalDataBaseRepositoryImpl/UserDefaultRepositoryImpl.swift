ff981174933345845160ec18958d5f8f2dbca303
sangheon <sangheon0724@gmail.com>
2023-11-21 11:06:14 +0900

    [v 1.0.0 ] [fix: 리뷰반영 - impl 구현 ]

--- iTime/Platform/Sources/LocalDataBaseRepositoryImpl/UserDefaultRepositoryImpl.swift
+++ iTime/Platform/Sources/LocalDataBaseRepositoryImpl/UserDefaultRepositoryImpl.swift
@@ -1,6 +1,6 @@
 //
 //  File.swift
-//  
+//
 //
 //  Created by 이상헌 on 11/20/23.
 //
@@ -11,40 +11,25 @@ import LocalDataBaseRepository
 
 // MARK: - UserDefaultRepositoryImpl
 
-final class UserDefaultRepositoryImpl: UserDefaultRepository  {
-  @UserDefaultWrapper(key: UserDefaultKey.userID.rawValue, defaultValue: String())
-  var userID: String
-  
-  @UserDefaultWrapper(key: UserDefaultKey.isAppBackground.rawValue, defaultValue: false)
-  var isAppBackground: Bool
-}
-// MARK: - UserDefaultWrapper
-
-@propertyWrapper
-struct UserDefaultWrapper<T: Codable> {
-  private let key: String
-  private let defaultValue: T
-  
-  init(key: String, defaultValue: T) {
-    self.key = key
-    self.defaultValue = defaultValue
+final class UserDefaultRepositoryImpl: WriteUserDefaultRepository, ReadOnlyUserDefaultRepository  {
+  func updateUserID(with userID: String) {
+    UserDefaults.standard.setValue(isAppBackground, forKey: UserDefaultKey.isAppBackground.rawValue)
   }
   
-  var wrappedValue: T {
-    get {
-      if let savedData = UserDefaults.standard.object(forKey: key) as? Data {
-        let decoder = JSONDecoder()
-        if let lodedObejct = try? decoder.decode(T.self, from: savedData) {
-          return lodedObejct
-        }
-      }
-      return defaultValue
+  func updateIsAppBackground(with isAppBackground: Bool) {
+    UserDefaults.standard.setValue(isAppBackground, forKey: UserDefaultKey.isAppBackground.rawValue)
+  }
+  
+  func userID() -> String {
+    guard let userID = UserDefaults.standard.string(forKey: UserDefaultKey.userID.rawValue) else {
+      assertionFailure(" userID is nil, default value is empty ")
+      return String()
     }
-    set {
-      let encoder = JSONEncoder()
-      if let encoded = try? encoder.encode(newValue) {
-        UserDefaults.standard.setValue(encoded, forKey: key)
-      }
-    }
+    return userID
+  }
+  
+  /// defualt value is false
+  func isAppBackground() -> Bool {
+    UserDefaults.standard.bool(forKey: UserDefaultKey.isAppBackground.rawValue)
   }
 }
