d4ba56393e59a40dc93fffee6ad222e17fe33ef8
sangheon <sangheon0724@gmail.com>
2023-12-08 00:26:31 +0900

    [v1.0.0] [feat: 행간 정리 ]

--- iTime/ProxyPackage/Sources/AppFoundation/LightReactor/AssociatedObjectStore.swift
+++ iTime/ProxyPackage/Sources/AppFoundation/LightReactor/AssociatedObjectStore.swift
@@ -1,20 +1,19 @@
 //
 //  File.swift
-//  
+//
 //
 //  Created by 이상헌 on 12/7/23.
 //
 
 import Foundation
 
-public protocol AssociatedObjectStore {
-}
+public protocol AssociatedObjectStore {}
 
 extension AssociatedObjectStore {
   func associatedObject<T>(forKey key: UnsafeRawPointer) -> T? {
     return objc_getAssociatedObject(self, key) as? T
   }
-
+  
   func associatedObject<T>(forKey key: UnsafeRawPointer, default: @autoclosure () -> T) -> T {
     if let object: T = self.associatedObject(forKey: key) {
       return object
@@ -23,7 +22,7 @@ extension AssociatedObjectStore {
     self.setAssociatedObject(object, forKey: key)
     return object
   }
-
+  
   func setAssociatedObject<T>(_ object: T?, forKey key: UnsafeRawPointer) {
     objc_setAssociatedObject(self, key, object, .OBJC_ASSOCIATION_RETAIN_NONATOMIC)
   }
