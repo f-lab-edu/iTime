fff9c1d977b120a2ea7699d9b9327040364546fd
sangheon <sangheon0724@gmail.com>
2024-01-19 23:20:27 +0900

    feat add withMainSerialExecutor in tests

--- iTime/Platform/Tests/RepositoryImplTests/RunningTimeTrackerImplTests.swift
+++ iTime/Platform/Tests/RepositoryImplTests/RunningTimeTrackerImplTests.swift
@@ -7,6 +7,7 @@
 import XCTest
 import RxSwift
 import Clocks
+import ConcurrencyExtras
 
 import Repository
 @testable import RepositoryImpl
@@ -38,92 +39,104 @@ final class RunningTimeTrackerImplTests: XCTestCase, @unchecked Sendable {
   }
   
   func test_Second_When_TimerStart() async throws {
-    // When
-    sut.start()
-    
-    // Then
-    await testClock.advance(by: .seconds(1))
-    
-    async let second = sut.currentSeconds().take(1).values
-    let result = try await second.first(where: { _ in true })
-    XCTAssertEqual(result, 1)
+    try await withMainSerialExecutor {
+      // When
+      sut.start()
+      
+      // Then
+      await testClock.advance(by: .seconds(1))
+      await Task.yield()
+      async let second = sut.currentSeconds().take(1).values
+      let result = try await second.first(where: { _ in true })
+      XCTAssertEqual(result, 1)
+    }
   }
   
   func test_10_Seconds_When_TimeStart() async throws {
-    // Given
-    let expectedSeconds = 10
-    
-    // When
-    sut.start()
-    
-    // Then
-    await testClock.advance(by: .seconds(expectedSeconds))
-    
-    async let seconds = sut.currentSeconds().take(1).values
-    let result = try await seconds.first(where: { _ in true })
-    XCTAssertEqual(result, expectedSeconds)
+    try await withMainSerialExecutor {
+      // Given
+      let expectedSeconds = 10
+      
+      // When
+      sut.start()
+      
+      // Then
+      await testClock.advance(by: .seconds(expectedSeconds))
+      
+      async let seconds = sut.currentSeconds().take(1).values
+      let result = try await seconds.first(where: { _ in true })
+      XCTAssertEqual(result, expectedSeconds)
+    }
   }
   
   
   func test_20_Seconds_When_TimeStart() async throws {
-    // Given
-    let expectedSeconds = 20
-    
-    // When
-    sut.start()
-    
-    // Then
-    await testClock.advance(by: .seconds(expectedSeconds))
-    
-    async let seconds = sut.currentSeconds().take(1).values
-    let result = try await seconds.first(where: { _ in true })
-    XCTAssertEqual(result, expectedSeconds)
+    try await withMainSerialExecutor {
+      // Given
+      let expectedSeconds = 20
+      
+      // When
+      sut.start()
+      
+      // Then
+      await testClock.advance(by: .seconds(expectedSeconds))
+      
+      async let seconds = sut.currentSeconds().take(1).values
+      let result = try await seconds.first(where: { _ in true })
+      XCTAssertEqual(result, expectedSeconds)
+    }
   }
   
   
   func test_100_Seconds_When_TimeStart() async throws {
-    // Given
-    let expectedSeconds = 100
-    
-    // When
-    sut.start()
-    
-    // Then
-    await testClock.advance(by: .seconds(expectedSeconds))
-    
-    async let seconds = sut.currentSeconds().take(1).values
-    let result = try await seconds.first(where: { _ in true })
-    XCTAssertEqual(result, expectedSeconds)
+    try await withMainSerialExecutor {
+      // Given
+      let expectedSeconds = 100
+      
+      // When
+      sut.start()
+      
+      // Then
+      await testClock.advance(by: .seconds(expectedSeconds))
+
+      async let seconds = sut.currentSeconds().take(1).values
+      let result = try await seconds.first(where: { _ in true })
+      XCTAssertEqual(result, expectedSeconds)
+    }
   }
   
   func test_1000_Seconds_When_TimeStart() async throws {
-    // Given
-    let expectedSeconds = 100 // TEMP: swift-clock PR 반영된후 반영
-    
-    // When
-    sut.start()
-    
-    // Then
-    await testClock.advance(by: .seconds(expectedSeconds))
-    
-    async let seconds = sut.currentSeconds().take(1).values
-    let result = try await seconds.first(where: { _ in true })
-    XCTAssertEqual(result, expectedSeconds)
+    try await withMainSerialExecutor {
+      // Given
+      let expectedSeconds = 1000
+      
+      // When
+      sut.start()
+      
+      // Then
+      await testClock.advance(by: .seconds(expectedSeconds))
+      
+      async let seconds = sut.currentSeconds().take(1).values
+      let result = try await seconds.first(where: { _ in true })
+      XCTAssertEqual(result, expectedSeconds)
+    }
   }
 
   func test_random_Seconds_When_TimeStart() async throws {
-    // Given
-    let randomSecond = Int.random(in: 1..<1000)
-    
-    // When
-    sut.start()
-    
-    // Then
-    await testClock.advance(by: .seconds(randomSecond))
-    
-    async let seconds = sut.currentSeconds().take(1).values
-    let result = try await seconds.first(where: { _ in true })
-    XCTAssertEqual(result, randomSecond)
+    try await withMainSerialExecutor {
+      // Given
+      let randomSecond = Int.random(in: 1..<1000)
+      
+      // When
+      sut.start()
+      
+      // Then
+      await testClock.advance(by: .seconds(randomSecond))
+      
+      async let seconds = sut.currentSeconds().take(1).values
+      let result = try await seconds.first(where: { _ in true })
+      XCTAssertEqual(result, randomSecond)
+    }
   }
   
   func test_Time_Passes_When_TimeStart() async throws {
